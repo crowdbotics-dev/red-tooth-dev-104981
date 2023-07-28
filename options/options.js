3dbe2ff5971fc597f8b2aa731ace64625f8b0ee7
Crowdbotics <engineering@crowdbotics.com>
2023-07-28 00:48:18 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "red-tooth-dev-104981",
+  url: "https://red-tooth-dev-104981.botics.co",
+  api: "https://red-tooth-dev-104981.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
